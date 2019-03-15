cfd71dd905ebdb35367d2ca4b4b2cfd458cfd602
jhansiblu <48461086+jhansiblu@users.noreply.github.com>
2019-03-15 12:40:57 -0400

    Update ATAnewscript.py

--- ATAnewscript.py
+++ ATAnewscript.py
@@ -13,7 +13,7 @@ print("I met some nice people at ATA. They are:")
 cur = con.cursor()
 
 #execute query
-cur.execute("select firstname, lastsname from "people".staff")
+cur.execute("select firstname, lastsname from people.staff")
 
 rows = cur.fetchall() 
 
