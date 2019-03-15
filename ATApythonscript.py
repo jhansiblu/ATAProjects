d83c08b54452bacefc28e3b81776d320803c8788
jhansiblu <48461086+jhansiblu@users.noreply.github.com>
2019-03-14 23:00:15 -0400

    Update ATApythonscript.py

--- ATApythonscript.py
+++ ATApythonscript.py
@@ -1,4 +1,35 @@
-#! /usr/bin/python
-Select firstname +' '+lastsname
-from "People".staff
-order by lastsname asc
+PGHOST=localhost
+PGDATABASE=ATAtestdb
+PGUSER=postgres
+PGPASSWORD="L!oness8"
+
+import psycopg2
+import sys, os
+import numpy as np
+import pandas as pd
+import example_psql as creds
+import pandas.io.sql as psql
+
+# Set up a connection to the postgres server.
+conn_string = "host="+ creds.PGHOST +" port="+ "5432" +" dbname="+ creds.PGDATABASE +" user=" + creds.PGUSER \
++" password="+ creds.PGPASSWORD
+conn=psycopg2.connect(conn_string)
+print("Connected!")
+
+
+# Create a cursor object
+cursor = conn.cursor()
+
+
+def load_data(schema, table):
+
+    sql_command = "SELECT * FROM {People}.{staff};".format(str(schema), str(table))
+    print (sql_command)
+
+    # Load the data
+    data = pd.read_sql(sql_command, conn)
+
+    print(data.shape)
+    return (data)
+
+
